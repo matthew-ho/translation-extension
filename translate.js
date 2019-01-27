c679fc0500249def9e2411f100498396e5749b05
MikeIsMike <mikexu22@hotmail.com>
2019-01-27 10:49:45 +0000

    new libraries implemented and used

--- translate.js
+++ translate.js
@@ -1,10 +1,9 @@
 //import {translations} from './translations.js'
-
 function translate(translations) {
   var translationsList = Object.entries(translations);
   for (var i = 0; i < translationsList.length; i++){
     var regex = "\\b" + translationsList[i][0] + "\\b";
-    var re = new RegExp(regex, "g");
+    var re = new RegExp(regex, "ig");
     findAndReplaceDOMText(document.body, {
       find: re,
       replace: translationsList[i][1]
@@ -12,5 +11,24 @@ function translate(translations) {
   }
 }
 
-//var translations = {"狗": "dog", "狼": "wolf"};
-translate(eToC);
+
+chrome.storage.sync.get('color', function(data) {
+  //changeColor.style.backgroundColor = data.color;
+  //changeColor.setAttribute('value', data.color);
+  console.log(data.color);
+  if (data.color == "#3aa757"){
+    translate(eToC1);
+  }
+  else if(data.color == "#f9bb2d"){
+    translate(eToC2);
+  }
+  else if(data.color == "#e8453c"){
+    translate(eToC3);
+  }
+
+});
+
+
+//
+// //var translations = {"狗": "dog", "狼": "wolf"};
+// translate(eToC);
